ecd13994a07a9fac2ef18f969340e312942f11e9
AbeerZahid917 <27100151@lums.edu.pk>
2025-06-02 11:30:41 +0500

    Video 47

--- inotebook/routes/auth.js
+++ inotebook/routes/auth.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const {body, validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
 
+//create a user using: POST "/api/auth/createuser". doesnt require authorization
 
-//create a user using: POST "/api/auth/". doesnt require authorization
-
-router.post('/', [ // this is an array part of the arguments
+router.post('/createuser', [ // this is an array part of the arguments
 
     body('name', 'Enter a valid name').isLength({min: 3}), // min length
     body('email', 'Enter a valid email').isEmail(), // checking if its an actual email, the second line shows when incorrect
@@ -23,15 +23,31 @@ router.post('/', [ // this is an array part of the arguments
             return res.status(400).json({errors: errors.array()});
         }
 
-        User.create ({
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email
-        })
-        .then(user => res.json(user))
-        .catch(err => {console.log(err)
-            res.json({error: 'Please enter a unique value for email'})}) // then and catch work like try, catch
-        
+        try
+        {
+            // check whether the email exists already
+            let user = await User.findOne({email: req.body.email});
+            if (user)
+            {
+                return res.status(400).json({error: "Sorry, a user with this email already exists"})
+            }
+
+            const salt = bcrypt.genSalt(10);
+            const sec_pass = bcrypt.hash(req.body.password, salt);
+
+            // creates a new user
+            user = await User.create ({
+                name: req.body.name,
+                password: sec_pass,
+                email: req.body.email
+            })
+            res.json({"res": "response"})
+        }
+        catch (error)
+        {
+            console.log(error.message);
+            res.status(500).send("some error occured");
+        }
 })
 
 module.exports = router;
